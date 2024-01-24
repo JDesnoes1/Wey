379ecf0d68c4a62f82893cabb3166ae657814c0f
Jérémie Desnoes <j.desnoes1@gmail.com>
2024-01-24 20:45:20 +0100

    style droite signupView

--- wey_frontend/src/views/SignupView.vue
+++ wey_frontend/src/views/SignupView.vue
@@ -9,11 +9,55 @@
           Tempore, harum!
         </p>
         <p class="font-bold">
-          Don't have an account ?
-          <a href="#" class="underline">Click here to create one !</a>
+          Already have an account ?
+          <a href="#" class="underline">Click here to login !</a>
         </p>
       </div>
     </div>
-    <div class="main-right">Right</div>
+    <div class="main-right">
+      <div class="p-12 bg-white border border-gray-200 rounded-lg">
+        <form class="space-y-6">
+          <div>
+            <label>Name</label><br />
+            <input
+              type="text"
+              placeholder="Your name"
+              class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg"
+            />
+          </div>
+
+          <div>
+            <label>E-mail</label><br />
+            <input
+              type="email"
+              placeholder="Your e-mail address"
+              class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg"
+            />
+          </div>
+
+          <div>
+            <label>Password</label><br />
+            <input
+              type="password"
+              placeholder="Your password"
+              class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg"
+            />
+          </div>
+          <div>
+            <label>Repeat password</label><br />
+            <input
+              type="password"
+              placeholder="Repeat your password"
+              class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg"
+            />
+          </div>
+          <div>
+            <button class="py-4 px-6 bg-purple-600 text-white rounded-lg">
+              Sign up
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
