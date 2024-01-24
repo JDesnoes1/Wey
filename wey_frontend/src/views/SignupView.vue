5b95a22f2a6e618c3e396ffcbd20a726078c5089
Jérémie Desnoes <j.desnoes1@gmail.com>
2024-01-24 20:36:03 +0100

    Route login et style gauche login page

---
+++ wey_frontend/src/views/SignupView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="max-w-7xl mx-auto grid grid-cols-2 gap-4">
+    <div class="main-left">
+      <div class="p-12 bg-white border border-gray-200 rounded-lg">
+        <h1 class="mb-6 text-2xl">Log in</h1>
+        <p class="mb-6 text-gray-500">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus,
+          inventore. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Tempore, harum!
+        </p>
+        <p class="font-bold">
+          Don't have an account ?
+          <a href="#" class="underline">Click here to create one !</a>
+        </p>
+      </div>
+    </div>
+    <div class="main-right">Right</div>
+  </div>
+</template>
