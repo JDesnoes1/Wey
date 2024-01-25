31bafc6601868e4c10d3f277076003baefdf0f4d
Jérémie Desnoes <j.desnoes1@gmail.com>
2024-01-25 09:31:45 +0100

    Style MessageView

--- wey_frontend/src/router/index.js
+++ wey_frontend/src/router/index.js
@@ -3,6 +3,7 @@ import HomeView from "../views/HomeView.vue";
 import SignupView from "../views/SignupView.vue";
 import LoginView from "../views/LoginView.vue";
 import FeedView from "../views/FeedView.vue";
+import MessageView from "../views/MessageView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,6 +12,11 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
+    },
+    {
+      path: "/message",
+      name: "message",
+      component: MessageView,
     },
     {
       path: "/feed",
