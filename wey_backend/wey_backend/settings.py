1cbb75298c691f892d17b2f489027fe1700fcbe0
Jérémie Desnoes <j.desnoes1@gmail.com>
2024-01-26 09:34:03 +0100

    config csrf et cors

--- wey_backend/wey_backend/settings.py
+++ wey_backend/wey_backend/settings.py
@@ -34,6 +34,14 @@ REST_FRAMEWORK = {
     )
 }
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:5173',
+]
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -43,11 +51,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'djangorestframework',
     'djangorestframework_simplejwt',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
