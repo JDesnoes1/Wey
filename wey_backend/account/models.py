0e262bbc524985c6bcbbc71b75f3ddd019106784
Jérémie Desnoes <j.desnoes1@gmail.com>
2024-01-26 09:48:30 +0100

    importation importante premier model

--- wey_backend/account/models.py
+++ wey_backend/account/models.py
@@ -1,3 +1,6 @@
+import uuid
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
