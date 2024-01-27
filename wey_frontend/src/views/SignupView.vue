b4078eb2d02415f92c3f5f717ef4a67076bdc076
Jérémie Desnoes <j.desnoes1@gmail.com>
2024-01-27 10:54:51 +0100

    Inscription front et back fonctionne

--- wey_frontend/src/views/SignupView.vue
+++ wey_frontend/src/views/SignupView.vue
@@ -18,11 +18,12 @@
     </div>
     <div class="main-right">
       <div class="p-12 bg-white border border-gray-200 rounded-lg">
-        <form class="space-y-6">
+        <form class="space-y-6" v-on:submit.prevent="submitForm">
           <div>
             <label>Name</label><br />
             <input
               type="text"
+              v-model="form.name"
               placeholder="Your name"
               class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg"
             />
@@ -32,6 +33,7 @@
             <label>E-mail</label><br />
             <input
               type="email"
+              v-model="form.email"
               placeholder="Your e-mail address"
               class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg"
             />
@@ -41,6 +43,7 @@
             <label>Password</label><br />
             <input
               type="password"
+              v-model="form.password1"
               placeholder="Your password"
               class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg"
             />
@@ -49,10 +52,18 @@
             <label>Repeat password</label><br />
             <input
               type="password"
+              v-model="form.password2"
               placeholder="Repeat your password"
               class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg"
             />
           </div>
+
+          <template v-if="errors.length > 0">
+            <div class="bg-red-300 text-white rounded-lg p-6">
+              <p v-for="error in errors" v-bind:key="error">{{ error }}</p>
+            </div>
+          </template>
+
           <div>
             <button class="py-4 px-6 bg-purple-600 text-white rounded-lg">
               Sign up
@@ -63,3 +74,76 @@
     </div>
   </div>
 </template>
+
+<script>
+import { useToastStore } from "@/stores/toast";
+import axios from "axios";
+
+export default {
+  setup() {
+    const toastStore = useToastStore();
+
+    return {
+      toastStore,
+    };
+  },
+
+  data() {
+    return {
+      form: {
+        email: "",
+        name: "",
+        password1: "",
+        password2: "",
+      },
+      errors: [],
+    };
+  },
+
+  methods: {
+    submitForm() {
+      this.errors = [];
+
+      if (this.form.email === "") {
+        this.errors.push("Your e-mail is missing");
+      }
+      if (this.form.name === "") {
+        this.errors.push("Your name is missing");
+      }
+      if (this.form.password1 === "") {
+        this.errors.push("Your password is missing");
+      }
+      if (this.form.password1 !== this.form.password2) {
+        this.errors.push("The passwords does not match");
+      }
+
+      if (this.errors.length === 0) {
+        axios
+          .post("/api/signup/", this.form)
+          .then((response) => {
+            if (response.data.message === "success") {
+              this.toastStore.showToast(
+                5000,
+                "The user is registered. Please log in",
+                "bg-emerald-500"
+              );
+              this.form.email = "";
+              this.form.name = "";
+              this.form.password1 = "";
+              this.form.password2 = "";
+            } else {
+              this.toastStore.showToast(
+                5000,
+                "Something went wrong. Please try again",
+                "bg-red-300"
+              );
+            }
+          })
+          .catch((error) => {
+            console.log("error", error);
+          });
+      }
+    },
+  },
+};
+</script>
